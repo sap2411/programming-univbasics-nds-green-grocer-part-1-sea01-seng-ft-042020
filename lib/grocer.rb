15fdc60b30525971de36a896316d1904766fcf6c
Steven Parsons <steven.parsons.us@gmail.com>
2020-04-17 21:14:15 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -16,7 +16,7 @@ def consolidate_cart(cart)
       key[:count] = 1 
       consolidated << key
     else
-      find_item_by_name_in_collection(key[:item], consolidated)
+      find_item_by_name_in_collection(key[:item], consolidated)[:count] +=1
   end
  end
 end
