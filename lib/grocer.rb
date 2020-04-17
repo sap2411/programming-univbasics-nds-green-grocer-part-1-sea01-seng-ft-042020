37c1b79cbbb8099405afd0e5d1fbfc6a820d5fdd
Steven Parsons <steven.parsons.us@gmail.com>
2020-04-17 21:01:30 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,8 +3,7 @@ require "pry"
 def find_item_by_name_in_collection(name, collection)
   collection.map do |key_num, value|
     if key_num[:item] === name
-      binding.pry
-      return value
+      return key_num
     end
   end
 end
